1725acf1b7b5e6cf6fecb8bd53ca0dd23ecd5161
aries grey <ariesgrey88@gmail.com>
2023-06-15 20:56:20 -0500

    completed caeser function and tests

--- src/caesar.js
+++ src/caesar.js
@@ -4,10 +4,41 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
-
+  // shift: positive number means shifting to the right (i.e., A becomes D) whereas a negative number means shifting to the left (i.e., M becomes K).
   function caesar(input, shift, encode = true) {
-    // your solution code here
+    // error handling
+    if (!shift || shift > 25 || shift < -25) return false;
+
+    input = input.toLowerCase();
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let shiftedAlphabet = "";
+    let result = "";
+
+    // shift opposite direction if decoding
+    if (encode === false) shift = 0 - shift;
+
+    // use substrings to create new shifted alphabet string, matching by index
+    if (shift > 0) {
+      shiftedAlphabet =
+        alphabet.substring(shift) + alphabet.substring(0, shift);
+    } else {
+      shiftedAlphabet =
+        alphabet.substring(26 + shift) + alphabet.substring(0, 26 + shift);
+    }
+
+    // replace each letter of input string with corresponding letter in shifted alphabet, using index
+    for (let i = 0; i < input.length; i++) {
+      const char = input[i];
+      const alphabetIndex = alphabet.indexOf(char);
+      // keep spaces and symbols the same
+      if (alphabetIndex === -1) {
+        result += char;
+      } else {
+        result += shiftedAlphabet[alphabetIndex];
+      }
+    }
+
+    return result;
   }
 
   return {
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,57 @@
-// Write your tests here!
+const { caesar } = require("../src/caesar");
+const { expect } = require("chai");
+
+describe("caeser", () => {
+  it("should return 'false' if the shift value is 0", () => {
+    const input = "test";
+    const shift = 0;
+    const actual = caesar(input, shift);
+    expect(actual).to.be.false;
+  });
+
+  it("should return 'false' if the shift value is > 25", () => {
+    const input = "test";
+    const shift = 26;
+    const actual = caesar(input, shift);
+    expect(actual).to.be.false;
+  });
+
+  it("should return 'false' if the shift value is < -25", () => {
+    const input = "test";
+    const shift = -26;
+    const actual = caesar(input, shift);
+    expect(actual).to.be.false;
+  });
+
+  it("should ignore capital letters", () => {
+    const input = "TESTing";
+    const shift = 1;
+    const actual = caesar(input, shift);
+    const expected = "uftujoh";
+    expect(actual).to.equal(expected);
+  });
+
+  it("should handle shifts that go past the end of the alphabet (right)", () => {
+    const input = "z";
+    const shift = 3;
+    const actual = caesar(input, shift);
+    const expected = "c";
+    expect(actual).to.equal(expected);
+  });
+
+  it("should handle shifts that go past the end of the alphabet (left)", () => {
+    const input = "a";
+    const shift = -3;
+    const actual = caesar(input, shift);
+    const expected = "x";
+    expect(actual).to.equal(expected);
+  });
+
+  it("should maintain spacing and other nonalphabetical symbols in the message", () => {
+    const input = "hello world!";
+    const shift = 1;
+    const actual = caesar(input, shift);
+    const expected = "ifmmp xpsme!";
+    expect(actual).to.equal(expected);
+  });
+});
