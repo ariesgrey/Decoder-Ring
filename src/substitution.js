c970c30db490157509b992ac079bc1dbdadfc5ce
aries grey <ariesgrey88@gmail.com>
2023-06-18 22:45:19 -0500

    completed substitution and substitution tests

--- src/substitution.js
+++ src/substitution.js
@@ -1,13 +1,43 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
-
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    // error handling
+    if (!alphabet || alphabet.length !== 26) return false;
+    for (let i = 0; i < 26; i++) {
+      const char = alphabet[i];
+      const rest = alphabet.substring(i + 1);
+      if (rest.includes(char)) return false;
+    }
+
+    // ignore capital letters
+    input = input.toLowerCase();
+    const regAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    let result = "";
+
+    // loop through input
+    for (let i = 0; i < input.length; i++) {
+      const char = input[i];
+
+      // whether encoding or decoding, maintain any spaces and skip to next character
+      if (char === " ") {
+        result += char;
+        continue;
+      }
+
+      let charIndex, newChar;
+      // encoding: use index of the character in the regular alphabet to find its match in the substitution alphabet
+      if (encode) {
+        charIndex = regAlphabet.indexOf(char);
+        newChar = alphabet[charIndex];
+        // decoding: use index of the character in the substitution alphabet to find its match in the regular alphabet
+      } else {
+        charIndex = alphabet.indexOf(char);
+        newChar = regAlphabet[charIndex];
+      }
+      // add matching character to result
+      result += newChar;
+    }
+
+    return result;
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,105 @@
-// Write your tests here!
+const { substitution } = require("../src/substitution");
+const { expect } = require("chai");
+
+describe("substitution", () => {
+  describe("error handling", () => {
+    it("should return 'false' if the substitution alphabet is missing", () => {
+      const input = "testing";
+      const actual = substitution(input);
+
+      expect(actual).to.be.false;
+    });
+
+    it("should return 'false' if the substitution alphabet is not exactly 26 characters", () => {
+      const input = "testing";
+      const alphabet = "xyz";
+      const actual = substitution(input, alphabet);
+
+      expect(actual).to.be.false;
+    });
+
+    it("should return 'false' if the substitution alphabet is not only unique characters", () => {
+      const input = "testing";
+      const alphabet = "xyzxyzxyzxyzxyzxyzxyzxyzxy";
+      const actual = substitution(input, alphabet);
+
+      expect(actual).to.be.false;
+    });
+  });
+
+  describe("encoding", () => {
+    it("should encode the message using the given substitution alphabet", () => {
+      const input = "testing";
+      const alphabet = "qwertyuiopasdfghjklzxcvbnm";
+      const actual = substitution(input, alphabet);
+      const expected = "ztlzofu";
+
+      expect(actual).to.equal(expected);
+    });
+
+    it("should work with non-letter symbols included in the substitution alphabet", () => {
+      const input = "testing";
+      const alphabet = "zaq!xsw@cde#vfr$bgt%nhy^mj";
+      const actual = substitution(input, alphabet);
+      const expected = "%xt%cfw";
+
+      expect(actual).to.equal(expected);
+    });
+
+    it("should maintain spaces in the message", () => {
+      const input = "hello world";
+      const alphabet = "qwertyuiopasdfghjklzxcvbnm";
+      const actual = substitution(input, alphabet);
+      const expected = "itssg vgksr";
+
+      expect(actual).to.equal(expected);
+    });
+
+    it("should ignore capital letters", () => {
+      const input = "TESTing";
+      const alphabet = "qwertyuiopasdfghjklzxcvbnm";
+      const actual = substitution(input, alphabet);
+      const expected = "ztlzofu";
+
+      expect(actual).to.equal(expected);
+    });
+  });
+
+  describe("decoding", () => {
+    it("should decode the message using the given substitution alphabet", () => {
+      const input = "ztlzofu";
+      const alphabet = "qwertyuiopasdfghjklzxcvbnm";
+      const actual = substitution(input, alphabet, false);
+      const expected = "testing";
+
+      expect(actual).to.equal(expected);
+    });
+
+    it("should work with non-letter symbols included in the substitution alphabet", () => {
+      const input = "%xt%cfw";
+      const alphabet = "zaq!xsw@cde#vfr$bgt%nhy^mj";
+      const actual = substitution(input, alphabet, false);
+      const expected = "testing";
+
+      expect(actual).to.equal(expected);
+    });
+
+    it("should maintain spaces in the message", () => {
+      const input = "itssg vgksr";
+      const alphabet = "qwertyuiopasdfghjklzxcvbnm";
+      const actual = substitution(input, alphabet, false);
+      const expected = "hello world";
+
+      expect(actual).to.equal(expected);
+    });
+
+    it("should ignore capital letters", () => {
+      const input = "ZTLZofu";
+      const alphabet = "qwertyuiopasdfghjklzxcvbnm";
+      const actual = substitution(input, alphabet, false);
+      const expected = "testing";
+
+      expect(actual).to.equal(expected);
+    });
+  });
+});
